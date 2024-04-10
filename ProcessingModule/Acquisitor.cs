a7aba9cf20e84542847d8545ff864ae2ee83b21f
Acile067 <acile067@gmail.com>
2024-04-10 11:16:53 +0200

    Acquisition_DoWork

--- ProcessingModule/Acquisitor.cs
+++ ProcessingModule/Acquisitor.cs
@@ -1,5 +1,6 @@
 ﻿using Common;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ProcessingModule
@@ -56,7 +57,20 @@ namespace ProcessingModule
         /// </summary>
 		private void Acquisition_DoWork()
 		{
-            //TO DO: IMPLEMENT
+            List<IConfigItem> config = configuration.GetConfigurationItems();
+            while (true)
+            {
+                acquisitionTrigger.WaitOne();
+                foreach (IConfigItem item in config)
+                {
+                    item.SecondsPassedSinceLastPoll++;
+                    if (item.SecondsPassedSinceLastPoll == item.AcquisitionInterval)
+                    {
+                        processingManager.ExecuteReadCommand(item, configuration.GetTransactionId(), configuration.UnitAddress, item.StartAddress, item.NumberOfRegisters);
+                        item.SecondsPassedSinceLastPoll = 0;
+                    }
+                }
+            }
         }
 
         #endregion Private Methods
